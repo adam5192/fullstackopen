96b88e2a8684ea08bcf8278b8bfa8f69a2b16056
adam5192 <adammokdad2003@gmail.com>
2025-08-06 17:46:04 -0400

    complete 2c

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Person from './components/Person'
+import axios from 'axios'
 
 const Filter = ( {text, value, handle} ) => (
   <div>
@@ -34,15 +35,20 @@ const Persons = ( {list} ) => (
 )
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [inputFilter, setInputFilter] = useState('')
+
+  const hook = () => {
+    console.log('effect')
+    axios.get('http://localhost:3001/persons').then(response => {
+      console.log('promise fulfilled')
+      setPersons(response.data)
+    })
+  }
+  useEffect(hook, [])
+  console.log('render', persons.length, 'person(s)')
 
   const handleNewName = (event) => {
     setNewName(event.target.value)
