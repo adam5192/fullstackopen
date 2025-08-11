5169d5e03f31f59313183812eb306303f847097b
adam5192 <adammokdad2003@gmail.com>
2025-08-11 13:19:34 -0400

    update phonebook

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,10 +2,6 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 
-const cors = require('cors')
-
-app.use(cors())
-
 app.use(express.json())
 app.use(express.static('dist'))
 
